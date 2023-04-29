3fee2d942a9eec5ddbd3b92a887e26c443001613
faustluck <faustluck91@gmail.com>
2023-04-30 01:30:05 +0300

    global timer

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,12 @@
 import { changeSlider } from "./pages.js";
 import { changeSlide, setControl } from "./carousel.js";
 
+let timerId;
+
 let observer = new MutationObserver(mutationRecords => {
-  let timerId;
   const className = mutationRecords[0].target.classList[0];
   const pageIndex = +className.split("_")[1];
-  if (pageIndex === 3 && !timerId) {
+  if (pageIndex === 3) {
     timerId = setInterval(changeSlide, 2000);
   }
   if (pageIndex !== 3) {
